039447660306c9beaaeb9c47d074573bb51980f6
nafisreza <bigganinafis@gmail.com>
2024-09-11 15:24:00 +0600

    feat: company schema created

---
+++ models/company.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    logo: {
+        type: String,
+        default: "",
+    },
+    website: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, {timestamps: true});
+
+const Company = mongoose.model("Company", companySchema);
