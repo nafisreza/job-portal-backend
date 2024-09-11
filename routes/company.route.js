5287b7a73c7017bb5619df58df590dbe98878bb9
nafisreza <bigganinafis@gmail.com>
2024-09-11 18:26:20 +0600

    fix: company update route

--- routes/company.route.js
+++ routes/company.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/register', isAuthenticated, registerCompany);
 router.get('/get', isAuthenticated, getCompany);
 router.get('/get/:id', isAuthenticated, getCompanyById);
-router.post('/update/:id', isAuthenticated, updateCompany);
+router.put('/update/:id', isAuthenticated, updateCompany);
 
 export default router;
