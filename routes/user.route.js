4bf1ba9aed2b2c20bd0af701cb114215263f99ee
nafisreza <bigganinafis@gmail.com>
2024-09-11 17:48:24 +0600

    feat: logout user api

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { register, login, updateProfile } from '../controllers/user.controller.js';
+import { register, login, logout, updateProfile } from '../controllers/user.controller.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.post('/profile/update', isAuthenticated, updateProfile);
 
 export default router;
