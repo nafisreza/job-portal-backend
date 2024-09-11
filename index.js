cec898f39766f69525a68bb3024344bb93e34e2c
nafisreza <bigganinafis@gmail.com>
2024-09-11 15:03:53 +0600

    feat: mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
 
 const app = express();
 
@@ -20,8 +24,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
