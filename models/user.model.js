afeaa0632701bfbc72c4b25fb598847ec56a3e52
nafisreza <bigganinafis@gmail.com>
2024-09-11 15:16:21 +0600

    feat: user schema created

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: Boolean,
+        enum: ["employee", "recruiter"],
+        required: true,
+    },
+    profile: {
+        bio: {
+        type: String,
+        },
+        skills: {
+        type: [
+            {
+            type: String,
+            },
+        ],
+        },
+        experience: {
+        type: String,
+        },
+        resume: {
+        type: String,
+        },
+        resumeName: {
+        type: String,
+        },
+        company: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Company",
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
+    },
+}, {timestamps: true});
